95778606f494115cb29ffcc6912e011d4780c153
AsfndAmin <99183246+AsfndAmin@users.noreply.github.com>
2022-03-29 15:32:47 +0500

    Create TaxSplitter.sol

---
+++ TaxSplitter.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (finance/PaymentSplitter.sol)
+
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/finance/PaymentSplitter.sol";
+
+contract PAYMENTS is PaymentSplitter {
+    
+    constructor (address[] memory _payees, uint256[] memory _shares) PaymentSplitter(_payees, _shares) payable {}
+    
+}
